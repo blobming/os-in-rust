e22da50875627c16533ba27d4a866864a6c53bc2
blboming <xjm.good@gmail.com>
2021-06-10 18:19:05 -0700

    vga text mode

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
@@ -13,13 +15,15 @@ static HELLO: &[u8] = b"Hello World!";
 #[no_mangle] //In compiler construction, name mangling (also called name decoration) is a technique used to solve various problems caused by the need to resolve unique names for programming entities in many modern programming languages.
 //extern "C" -> tell the compiler that it should use the C calling convention for this function (instead of the unspecified Rust calling convention)
 pub extern "C" fn _start() -> ! { //default entrypoint
-    let vga_buffer = 0xb8000 as *mut u8;
+    // let vga_buffer = 0xb8000 as *mut u8;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    // for (i, &byte) in HELLO.iter().enumerate() {
+    //     unsafe {
+    //         *vga_buffer.offset(i as isize * 2) = byte;
+    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    //     }
+    // }
+    println!("Hello World{}","!");
+    panic!("Some panic message");
     loop {}
 }
